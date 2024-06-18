a874cdf1bf8c9679dbd3820e30a4bfa9cd5fb5ae
Kumar Shivesh <116376181+kumarshivesh@users.noreply.github.com>
2024-06-18 22:18:16 +0530

    Added styling to buttons

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const ArticleCard = ({ article, onSaveToFavorites, onRemoveFromFavorites }) => {
-  const [imgSrc, setImgSrc] = React.useState(article.urlToImage);
-
+  
   if (
     article.title === '[Removed]' || 
     article.description === '[Removed]' || 
@@ -11,28 +10,22 @@ const ArticleCard = ({ article, onSaveToFavorites, onRemoveFromFavorites }) => {
     return null;
   }
 
-  const handleError = () => {
-    setImgSrc('/image-not-found-1150x647.png'); // Use the path relative to the public folder
-  };
-
-  const isExternal = article.url.startsWith('http');
-
   return (
     <div className="article-card">
-      <img src={imgSrc} alt={article.title} onError={handleError} />
+      <div className='image-container'>
+        <img src={article.urlToImage} alt={article.title} />
+      </div>
       <h2>{article.title}</h2>
       <p>{article.description}</p>
-      {isExternal ? (
-        <a href={article.url} target="_blank" rel="noopener noreferrer">Read More</a>
-      ) : (
-        <a href={`/article/${encodeURIComponent(article.url)}`}>Read More</a> // Replaced Link with a tag
-      )}
-      {onSaveToFavorites && (
-        <button onClick={() => onSaveToFavorites(article)}>Save to Favorites</button>
-      )}
-      {onRemoveFromFavorites && (
-        <button onClick={() => onRemoveFromFavorites(article.url)}>Remove</button>
-      )}
+      <div className='botton-container'>
+        <a className='button' href={article.url} target="_blank" rel="noopener noreferrer">Read More</a>
+        {onSaveToFavorites && (
+          <button className='button' onClick={() => onSaveToFavorites(article)}>Save to Favorites</button>
+        )}
+        {onRemoveFromFavorites && (
+          <button className='button remove' onClick={() => onRemoveFromFavorites(article.url)}>Remove</button>
+        )}
+      </div>
     </div>
   );
 };
